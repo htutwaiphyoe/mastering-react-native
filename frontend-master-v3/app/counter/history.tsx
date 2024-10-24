573a69898c3f2cb6984bff76e2fdac0690871051
Damiano <htutwaiphyoe@gmail.com>
2024-10-25 03:17:43 +0700

    feat(frontend-master-v3): History

--- frontend-master-v3/app/counter/history.tsx
+++ frontend-master-v3/app/counter/history.tsx
@@ -1,20 +1,73 @@
-import { Text, View, StyleSheet } from "react-native";
+import { theme } from "theme";
+import { format } from "date-fns";
+import { getStorage } from "utils/storage";
+import { useEffect, useState } from "react";
+import { TPersistCountdownState } from "types";
+import { Text, StyleSheet, FlatList, View } from "react-native";
+import { COUNTDOWN_STORAGE_KEY, fullDateFormat } from "constant";
 
 export default function History() {
+  const { countDown } = useContainer();
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>History</Text>
-    </View>
+    <FlatList
+      style={styles.container}
+      data={countDown?.completedAt}
+      contentContainerStyle={styles.contentContainer}
+      renderItem={({ item }) => (
+        <View style={styles.listItem}>
+          <Text style={styles.listItemText}>
+            {format(item, fullDateFormat)}
+          </Text>
+        </View>
+      )}
+      ListEmptyComponent={
+        <View style={styles.listEmptyContainer}>
+          <Text style={styles.listItemText}>Countdown list is empty</Text>
+        </View>
+      }
+    />
   );
 }
+
+const useContainer = () => {
+  const [countDown, setCountDown] = useState<TPersistCountdownState>();
+
+  useEffect(() => {
+    const initCountdown = async () => {
+      const data = await getStorage(COUNTDOWN_STORAGE_KEY);
+      setCountDown(data);
+    };
+    initCountdown();
+  }, []);
+
+  return {
+    countDown,
+  };
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: theme.colorWhite,
+  },
+  contentContainer: {
+    marginTop: 8,
+  },
+  listEmptyContainer: {
     justifyContent: "center",
     alignItems: "center",
+    marginVertical: 18,
   },
-  text: {
-    fontSize: 24,
+  listItem: {
+    marginHorizontal: 8,
+    marginBottom: 8,
+    alignItems: "center",
+    backgroundColor: theme.colorLightGrey,
+    padding: 12,
+    borderRadius: 6,
+  },
+  listItemText: {
+    fontSize: 18,
   },
 });
--- frontend-master-v3/constant/index.ts
+++ frontend-master-v3/constant/index.ts
@@ -1,3 +1,5 @@
 export const interval = 5 * 1000;
 
 export const COUNTDOWN_STORAGE_KEY = "countdown";
+
+export const fullDateFormat = `LLL d yyyy, h:mm aaa`;
