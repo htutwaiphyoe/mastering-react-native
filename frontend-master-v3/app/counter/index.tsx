5f18c05f33fe98d07073fbb8415de13a15ba5513
Damiano <htutwaiphyoe@gmail.com>
2024-10-25 01:00:06 +0700

    feat(frontend-master-v3): Notifications

--- frontend-master-v3/app/counter/index.tsx
+++ frontend-master-v3/app/counter/index.tsx
@@ -1,10 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
+import Button from "components/Button";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import { theme } from "theme";
+import { registerPushNotifications } from "utils/notification";
+import * as Notifications from "expo-notifications";
 
 export default function Counter() {
+  const scheduleNotifications = async () => {
+    const result = await registerPushNotifications();
+    if (result === "granted") {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Hello, React Native!",
+        },
+        trigger: {
+          seconds: 3,
+        },
+      });
+    } else {
+      Alert.alert(
+        "Unable to schedule notification",
+        "Enable the notifications permission for Expo Go in settings"
+      );
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Counter</Text>
+      <Button style={styles.button} onPress={scheduleNotifications}>
+        <Text style={styles.buttonText}>Schedule Notifications</Text>
+      </Button>
     </View>
   );
 }
@@ -16,7 +39,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: theme.colorWhite,
   },
-  text: {
-    fontSize: 24,
+  button: {
+    padding: 12,
+    borderRadius: 6,
+    backgroundColor: theme.colorBlack,
+  },
+  buttonText: {
+    color: "#fff",
+    letterSpacing: 1,
+    fontWeight: "bold",
+    textTransform: "uppercase",
   },
 });
