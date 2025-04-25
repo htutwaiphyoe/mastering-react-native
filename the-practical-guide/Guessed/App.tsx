6ee1b5b5f473451bc7a251f963708372ca89d0d0
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-04-25 22:20:46 +0700

    feat(the-practical-guide): Guessed project setup

---
+++ the-practical-guide/Guessed/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Ok</Text>
+    </View>
+  );
+}
+
+export default App;
