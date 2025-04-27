88bd1c774249138ee726fd1e670d6cd031638bf4
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-04-27 21:08:57 +0700

    feat(the-practical-guide): start screen game logic

--- the-practical-guide/Guessed/screens/StartScreen/index.tsx
+++ the-practical-guide/Guessed/screens/StartScreen/index.tsx
@@ -1,18 +1,43 @@
-import React from 'react';
-import {TextInput, View} from 'react-native';
 import {styles} from './style';
 import {Button} from '../../components/Button';
+import React, {useCallback, useState} from 'react';
+import {Alert, TextInput, View} from 'react-native';
 
 export function StartScreen() {
+  const [number, setNumber] = useState('');
+
+  const handleReset = useCallback(() => setNumber(''), []);
+
+  const handleConfirm = () => {
+    const isValidNumber =
+      !isNaN(Number(number)) && Number(number) > 0 && Number(number) < 100;
+
+    if (!isValidNumber) {
+      Alert.alert('Invalid number!', 'Number must be between 1 and 99.', [
+        {
+          text: 'Try again',
+          style: 'destructive',
+          onPress: handleReset,
+        },
+      ]);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <TextInput maxLength={2} keyboardType="number-pad" style={styles.input} />
+      <TextInput
+        maxLength={2}
+        value={number}
+        style={styles.input}
+        onChangeText={setNumber}
+        keyboardType="number-pad"
+      />
       <View style={styles.buttonContainer}>
         <View style={styles.button}>
-          <Button>Reset</Button>
+          <Button onPress={handleReset}>Reset</Button>
         </View>
         <View style={styles.button}>
-          <Button>Confirm</Button>
+          <Button onPress={handleConfirm}>Confirm</Button>
         </View>
       </View>
     </View>
